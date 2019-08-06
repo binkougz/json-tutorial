b2a8ddc17290758470c5aa691b5b935fbcce3838
Bruce Kou <binkougz@yahoo.com>
2019-08-06 19:52:04 +0800

    fix issue #01, nonempty after value

--- tutorial01/leptjson.c
+++ tutorial01/leptjson.c
@@ -34,11 +34,21 @@ static int lept_parse_value(lept_context* c, lept_value* v) {
 
 int lept_parse(lept_value* v, const char* json) {
     lept_context c;
+    int ret;
     assert(v != NULL);
     c.json = json;
     v->type = LEPT_NULL;
     lept_parse_whitespace(&c);
-    return lept_parse_value(&c, v);
+    ret = lept_parse_value(&c, v);
+
+    /* fix the for the issue #01 of having char after `value`. */
+    if (ret == LEPT_PARSE_OK) {
+        lept_parse_whitespace(&c);
+        if (c.json[0] != '\0') 
+        return LEPT_PARSE_ROOT_NOT_SINGULAR;
+    }
+    
+    return ret; 
 }
 
 lept_type lept_get_type(const lept_value* v) {
--- tutorial01/leptjson.h
+++ tutorial01/leptjson.h
@@ -1,12 +1,41 @@
-#ifndef LEPTJSON_H__
-#define LEPTJSON_H__
+#ifndef LEPTJSON_H__ /* include guard */
+#define LEPTJSON_H__ /* ends with `_H__` is just a recommendation */
 
-typedef enum { LEPT_NULL, LEPT_FALSE, LEPT_TRUE, LEPT_NUMBER, LEPT_STRING, LEPT_ARRAY, LEPT_OBJECT } lept_type;
+/*
+ *  typedef is essentially an alias, as to the compiler, there is 
+ *  actually the same type. 
+ *   
+ *  to eliminate the risk of naming collsion, we normally put the 
+ *  project name as prefix of each enum element.
+ *   
+ *  enum elem are all caps, type and function names are low case instead.
+ *   
+ *  notice that in c library, they use _t convention as like `size_t`   
+ */
+typedef enum { 
+    LEPT_NULL, 
+    LEPT_FALSE, 
+    LEPT_TRUE, 
+    LEPT_NUMBER, 
+    LEPT_STRING, 
+    LEPT_ARRAY, 
+    LEPT_OBJECT 
+} lept_type;
 
+/*
+ * for each node in the memory tree, we use the lept_value struct to 
+ * represent the value node.
+ *
+ */
 typedef struct {
     lept_type type;
-}lept_value;
+} lept_value;
 
+/*
+ * enum of parsing result 
+ *
+ *
+ */
 enum {
     LEPT_PARSE_OK = 0,
     LEPT_PARSE_EXPECT_VALUE,
@@ -14,8 +43,19 @@ enum {
     LEPT_PARSE_ROOT_NOT_SINGULAR
 };
 
+/*
+ * json param is an c style string, we are not supposed to change
+ * it, so let it const.
+ * 
+ * the interface is expecting storage is from client code.
+ * so v is an pointer of an node struct.
+ */
 int lept_parse(lept_value* v, const char* json);
 
+/*
+ * access the memory tree and returns the json value type
+ * as we defined in the beginning.
+ */
 lept_type lept_get_type(const lept_value* v);
 
 #endif /* LEPTJSON_H__ */
